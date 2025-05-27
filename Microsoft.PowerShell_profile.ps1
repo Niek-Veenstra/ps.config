e75fe0ed437b54340c9248c557e7871ea8f0ac99
niek veenstra <niekveenstra14@gmail.com>
2025-05-27 17:49:16 +0200

    Added some new aliases and maps.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,36 +1,70 @@
-function nrd(){
-	npm run dev
-}
-
-Set-Alias c clear
-
+#Region Utility.
+Set-Alias -Name c -Value clear
+Set-Alias -Name reload -Value ReloadProfile
 function clipboard(){
 	Set-Clipboard @args
 }
+function ReloadProfile() {
+    . $profile
+}
+function rmrf([string]$path) {
+    Remove-Item -Recurse -Force $path
+}
+function touch(){
+	New-Item @args
+}
+function usbs(){
+	Get-WMIObject Win32_SerialPort | Select-Object DeviceID, Description
+}
+#End region Utility.
 
+#Region NPM.
+function nrd(){
+	npm run dev
+}
+#End region NPM.
+
+#Region PHP/Laravel.
 function art() {
 	php artisan @args
 }
+#End region PHP/Laravel.
 
+#Region mysc.
 $rp_ip = "172.18.0.1"
 function rpi(){
      ssh "niekv@$rp_ip"
 }
+#End region mysc.
 
-function ReloadProfile() {
-    . $profile
+#Region GIT.
+function pull(){
+	git pull @args
 }
 
-Set-Alias -Name reload -Value ReloadProfile
-
-function rmrf([string]$path) {
-    Remove-Item -Recurse -Force $path
+function checkout(){
+	git checkout @args
 }
 
-function touch(){
-	New-Item @args
+function commit(){
+	git commit @args
 }
 
-function usbs(){
-	Get-WMIObject Win32_SerialPort | Select-Object DeviceID, Description
+function add(){
+	git add @args
 }
+
+function branch(){
+	git branch @args
+}
+
+function push(){
+	git push @args
+}
+function merge(){
+	git merge @args
+}
+#End region Git.
+
+Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
+oh-my-posh init pwsh --config ~/pwsh10k.omp.json | Invoke-Expression
